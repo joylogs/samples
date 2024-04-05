86dbc8ec041223100d903a9e430242068857ed12
Joy <joy.logs@gmail.com>
2024-04-05 22:55:33 +0530

    Adding Quake Location

--- Earthquakes/Earthquakes/Models/QuakeLocation.swift
+++ Earthquakes/Earthquakes/Models/QuakeLocation.swift
@@ -24,12 +24,29 @@ struct QuakeLocation: Decodable {
         var properties: OriginProperties
     }
     
-    struct OriginProperties: Decodable {
+    struct OriginProperties {
         var latitude: Double
         var longitude: Double
     }
 }
 
 extension QuakeLocation.OriginProperties: Decodable {
-//    private enum 
+    
+    private enum OriginPropertiesCodingKeys: String, CodingKey {
+        case latitude
+        case longitude
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: OriginPropertiesCodingKeys.self)
+        let longitude = try container.decode(String.self, forKey: .longitude)
+        let latitude = try container.decode(String.self, forKey: .latitude)
+        
+        guard let longitude = Double(longitude),
+                let latitude = Double(latitude)
+        else { throw QuakeError.missingData }
+        
+        self.longitude = longitude
+        self.latitude = latitude
+    }
 }
