8c99e9b5047c5bcbc7937be3b31c675f6b973569
Joy <joy.logs@gmail.com>
2024-04-05 23:13:27 +0530

    adding properties nested map for latitude, longitude

--- Earthquakes/Earthquakes/Models/QuakeLocation.swift
+++ Earthquakes/Earthquakes/Models/QuakeLocation.swift
@@ -8,8 +8,9 @@
 import Foundation
 
 struct QuakeLocation: Decodable {
-    var latitude: Double
-    var longitude: Double
+    var latitude: Double { properties.products.origin.first!.properties.latitude }
+    var longitude: Double { properties.products.origin.first!.properties.longitude }
+    
     private var properties: RootProperties
     
     struct RootProperties: Decodable {
