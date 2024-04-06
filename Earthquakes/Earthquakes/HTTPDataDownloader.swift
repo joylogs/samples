84767f862f3b393688d31182c5a55ac919282f93
Joy <joy.logs@gmail.com>
2024-04-06 23:59:07 +0530

    adding HTTP async data downloader class

---
+++ Earthquakes/Earthquakes/HTTPDataDownloader.swift
@@ -0,0 +1,23 @@
+//
+//  HTTPDataDownloader.swift
+//  Earthquakes
+//
+//  Created by Joy Banerjee on 06/04/24.
+//
+
+import Foundation
+
+let validStatus = 200...299
+
+protocol HTTPDataDownloader {
+    func httpData(from: URL) async throws -> Data
+}
+
+extension URLSession: HTTPDataDownloader {
+    func httpData(from url: URL) async throws -> Data {
+        guard let (data, response) = try await self.data(from: url, delegate: nil) as? (Data, HTTPURLResponse), validStatus.contains(response.statusCode) else {
+            throw QuakeError.networkError
+        }
+        return data
+    }
+}
