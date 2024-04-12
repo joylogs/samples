6e889352098e2d5dba46c8911afa0873dab4a572
Joy <joy.logs@gmail.com>
2024-04-12 23:48:25 +0530

    adding QuakeTest data

---
+++ Earthquakes/Earthquakes/Resources/QuakeTestData.swift
@@ -0,0 +1,21 @@
+//
+//  QuakeTestData.swift
+//  Earthquakes
+//
+//  Created by Joy Banerjee on 12/04/24.
+//
+
+import Foundation
+
+let staticData: [Quake] = [
+    Quake(magnitude: 0.8,
+          place: "Shakey Acres",
+          time: Date(timeIntervalSinceNow: -1000),
+          code: "nc73649170",
+          detail: URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nc73649170.geojson")!),
+    Quake(magnitude: 2.2,
+          place: "Rumble Alley",
+          time: Date(timeIntervalSinceNow: -5000),
+          code: "hv72783692",
+          detail: URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/hv72783692")!)
+]
--- Earthquakes/Earthquakes/Views/Quakes.swift
+++ Earthquakes/Earthquakes/Views/Quakes.swift
@@ -7,19 +7,6 @@
 
 import SwiftUI
 
-let staticData: [Quake] = [
-    Quake(magnitude: 0.8,
-          place: "Shakey Acres",
-          time: Date(timeIntervalSinceNow: -1000),
-          code: "nc73649170",
-          detail: URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nc73649170.geojson")!),
-    Quake(magnitude: 2.2,
-          place: "Rumble Alley",
-          time: Date(timeIntervalSinceNow: -5000),
-          code: "hv72783692",
-          detail: URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/hv72783692")!)
-]
-
 struct Quakes: View {
     @AppStorage("lastUpdated")
     var lastUpdated = Date.distantFuture.timeIntervalSince1970
